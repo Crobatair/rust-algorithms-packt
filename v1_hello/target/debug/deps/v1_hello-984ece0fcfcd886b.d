6d20fe5a3866abe74c7c7516f1bffe38c835ce1e
Alejandro Vásquez Madrid <jose5m45@gmail.com>
2021-02-18 18:11:08 -0600

    hello world

---
+++ v1_hello/target/debug/deps/v1_hello-984ece0fcfcd886b.d
@@ -0,0 +1,5 @@
+/home/ale/Documents/packt/algorithms-rust/v1_hello/target/debug/deps/v1_hello-984ece0fcfcd886b: src/main.rs
+
+/home/ale/Documents/packt/algorithms-rust/v1_hello/target/debug/deps/v1_hello-984ece0fcfcd886b.d: src/main.rs
+
+src/main.rs:
---
+++ v1_hello/target/debug/v1_hello.d
@@ -0,0 +1,1 @@
+/home/ale/Documents/packt/algorithms-rust/v1_hello/target/debug/v1_hello: /home/ale/Documents/packt/algorithms-rust/v1_hello/src/main.rs
