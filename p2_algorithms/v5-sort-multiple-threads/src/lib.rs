ba31524567f7427ad6f759a5ac9dec1706182ebc
Alejandro Vásquez Madrid <jose5m45@gmail.com>
2021-02-20 13:55:43 -0600

    Changes to be committed: 	modified:   ../v5-sort-multiple-threads/src/lib.rs 	new file:   Cargo.toml 	new file:   src/lib.rs

--- p2_algorithms/v5-sort-multiple-threads/src/lib.rs
+++ p2_algorithms/v5-sort-multiple-threads/src/lib.rs
@@ -98,7 +98,6 @@ mod tests {
         assert_eq!(v, vec![1, 2, 3, 4, 5, 6, 7]);
     }
 
-
     #[test]
     fn test_threaded_quick_sort() {
         let mut v = vec![4, 6, 1, 8, 11, 13, 3];
@@ -106,8 +105,4 @@ mod tests {
         assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13]);
         panic!();
     }
-
-
-
-
 }
