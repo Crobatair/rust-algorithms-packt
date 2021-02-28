f91acb0c5dfef4841691fbe27e60a11af73aa2ec
Alejandro Vásquez Madrid <jose5m45@gmail.com>
2021-02-28 14:16:11 -0600

    added commens to graph

--- p4_connected_nodes/v2_graphs/src/main.rs
+++ p4_connected_nodes/v2_graphs/src/main.rs
@@ -20,11 +20,18 @@ impl GraphErr {
 // Mappointer based
 #[derive(Debug)]
 pub struct Graph<T, E, ID: Hash + Eq>{
+    // data contains a hashmap, each element with an ID as Key
+    // and each contains a ( data, array of ids
     data: HashMap<ID,(T, Vec<ID>)>,
+    // each edge contains a ID, and a tuple of ( E, ID, ID)
+    // where ID, ID is 'from' and ID is 'to'
+    // {ID, (E, from, to)}
     edges: HashMap<ID, (E, ID, ID)>,
 }
 
+// any ID must implement trait eq, hash and Clone
 impl <T, E, ID: Clone+Hash + Eq> Graph<T, E, ID>{
+
     pub fn new()-> Self {
         Graph{
             data: HashMap::new(),
@@ -32,32 +39,39 @@ impl <T, E, ID: Clone+Hash + Eq> Graph<T, E, ID>{
         }
     }
 
-
     pub fn add_node(&mut self, id: ID, data: T) {
+        // because id is an ID, it will insert a new on hashmap, it element is duplicated, it will not added
         self.data.insert(id, (data, Vec::new()));
     }
 
-
+    // will add and edge on node
+    // return Ok or GraphErr type
     pub fn add_edge(&mut self, ed_id:ID, from: ID, to:ID, edat: E)-> Result<(), GraphErr>{
+        // if 'from' dont exist on data, because parent node not exist
         if ! self.data.contains_key(&from){
             return Err(GraphErr::new("'from' not in does"));
         }
+
+        // test if data contains to edge, if not return a Err() of type GraphErr
         if let Some(ref mut dt) = self.data.get_mut(&to){
+            // get as mut reference because we need to increse array of  references on child graph
+            // added new edge reference
+            // all cloned because they be reused
             self.edges.insert(ed_id.clone(),(edat, from.clone(), to.clone()));
+            // add to array of references
             dt.1.push(ed_id.clone());
         } else {
             return Err(GraphErr::new("'to' not in nodes"));
         }
 
-
+        // if from and to exists, add added the edge and  reference,
+        // then, get the reference of from *prev used to
+        // then push to refs edge id
         self.data.get_mut(&from).unwrap().1.push(ed_id.clone());
         Ok(())
-
     }
 
 }
-
-
 
 fn main() -> Result<(), GraphErr> {
 
