92af7d90069e1e8994b9b51d9a997af126882c26
Alejandro Vásquez Madrid <jose5m45@gmail.com>
2021-02-28 14:58:03 -0600

    added comments to router

--- p4_connected_nodes/v3_router_structure/src/main.rs
+++ p4_connected_nodes/v3_router_structure/src/main.rs
@@ -1,19 +1,22 @@
 mod errors;
 use std::rc::Rc;
-use std::collections::HashMap;
-use std::hash::Hash;
 use std::fmt;
 
+// create a trait to use on objects
 pub trait Weighted{
     fn weight(&self) ->i32;
 }
 
+// implement trait for any i32 object
 impl Weighted for i32 {
+
+    // named as weight will return a reference to a i32
     fn weight(&self) ->i32 {
         *self
     }
 }
 
+// added a struct Route, that will hold, pos, a path with a Ref Counter to a Route ID and a len
 #[derive(Debug)]
 pub struct Route<ID>{
     pos: ID,
@@ -21,6 +24,7 @@ pub struct Route<ID>{
     len: i32,
 }
 
+// Impl of Route, to wrap rc 
 impl <ID: Eq> Route<ID> {
     pub fn start_rc(pos: ID) -> Rc<Self> {
         Rc::new(Route {
@@ -30,12 +34,15 @@ impl <ID: Eq> Route<ID> {
         })
     }
 
+    // impl of contains for Route<ID>, return a bool with result
     pub fn contains(&self, id: &ID) -> bool {
 
+        // current pos eqls to compared id
         if self.pos == *id {
             return true;
         }
 
+        // if path contains on some place the id, then will return the result
         match self.path {
             Some(ref p) => p.contains(id),
             None=> false,
@@ -43,6 +50,7 @@ impl <ID: Eq> Route<ID> {
     }
 }
 
+// formatter Display for Route<ID>
 impl <ID:fmt::Debug> fmt::Display for Route<ID> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 
