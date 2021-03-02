c1dad3010af12a4976785f64dc6b1382e4d50a2e
Alejandro Vásquez Madrid <jose5m45@gmail.com>
2021-03-01 22:08:07 -0600

    added comments to hashmap impl of growing hashmap and dynamic grow

--- p5_hashmap/v3_finish_hashmap/src/lib.rs
+++ p5_hashmap/v3_finish_hashmap/src/lib.rs
@@ -90,15 +90,18 @@ impl <K: Hash + Eq, V> BucketList<K, V> {
         }
     }
 }
-
-
+//////////////////////////////////////// 
+// created the HMap to be used
 #[derive(Debug)]
 pub struct HMap<K, V>{
     n_moved: usize,
+    // declared to be the current main bucketlist in use... to avoid colision of use main while adding some buckets
     main: BucketList<K, V>,
+    // declared to hold temporary buckets
     grow: BucketList<K, V>,
 }
 
+// impl new to any K that traits Hash and Eq
 impl <K: Hash + Eq, V> HMap<K, V> {
     pub fn new() -> Self {
         HMap {
@@ -108,17 +111,23 @@ impl <K: Hash + Eq, V> HMap<K, V> {
         }
     }
 
+    // insert to main of hashmap
     pub fn insert(&mut self, k: K, v: V){
+        // if k exists on main, then put v with new data
+        // as standar, is changed the reference point with new, and returned
         if let Some(iv) = self.main.get_mut(&k){
             *iv = v;
             return;
         }
 
+        // if not exists on main, it can be on grow list, because remains to move to main array
         if let Some(iv) = self.main.get_mut(&k){
             *iv = v;
             return;
         }
 
+        // this means that we have some elemetns are on trasit?
+        // and we push to grow, and call move buckets, to make grow into main
         if self.n_moved > 0 {
             // we have started move to bigger bucket list
             self.grow.push(k, v);
@@ -127,18 +136,24 @@ impl <K: Hash + Eq, V> HMap<K, V> {
             return;
         }
 
+        // if all other fails...
+        // element must be added
+        // so we call push to main, and it returbs a usize
+        // if the main is higher than half of growing capacity, will move buckets 
         if self.main.push(k, v) > ( BSIZE/2 ){
             self.move_bucket();
         }
 
+
     }
 
+    // get the borrow optional for Key
     pub fn get<KR>(&self, kr: &KR) -> Option<&V> where K:Borrow<KR>, KR: Hash + Eq + ?Sized{
         // because we only wants the not mut part of object, we can shorten with that 
         self.main.get(kr).or_else(||self.grow.get(kr))
     }
 
-
+    // get the value element as a mutable reference.
     pub fn get_mut<KR>(&mut self, kr: &KR) -> Option<&mut V> where K:Borrow<KR>, KR: Hash + Eq + ?Sized{
         if let Some(b) = self.main.get_mut(kr){
             return Some(b);
@@ -146,15 +161,19 @@ impl <K: Hash + Eq, V> HMap<K, V> {
         self.grow.get_mut(kr)
     }
 
+    // just in case that can be needed
     pub fn len(&self)-> usize {
         self.main.len + self.grow.len
     }
 
+    // move bucket method
     pub fn move_bucket(&mut self){
+        // if you dont have any to move, just create buckets to grow main hashmap *high cost op, when the hashmap grows, so space already reserved to grow BGROW/
         if self.n_moved == 0 {
             self.grow.set_buckets(self.main.buckets.len() + BGROW);
         }
-        // i will be not owned bny main when used
+        
+        // i will be not owned by main when used
         if let Some(b) = self.main.bucket(self.n_moved){
             for (k, v) in b {
                 self.grow.push(k, v);
